d45c7923a6294c36293f34f133830fcf34813a39
Tunitski Uladzislau <tynvlad@gmail.com>
2023-03-23 12:00:11 +0300

    changes

---
+++ README.md
@@ -0,0 +1,3 @@
+https://github.com/robin-dela/hover-effect
+
+https://www.youtube.com/watch?v=3RNzWyXigMc
---
+++ app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+  parent: document.querySelector(".img"),
+  image1: "./one.png",
+  image2: "./sec.png",
+  displacementImage: "./displacement/dot.jpg",
+  imagesRatio: ".83",
+});
